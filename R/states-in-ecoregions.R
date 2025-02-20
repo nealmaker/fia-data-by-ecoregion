7c3ae8141c11f919c15ffdb5dab7d4f3c59619d1
Neal Maker <nealmaker@gmail.com>
2025-02-20 16:53:38 -0500

    add state and ecoregion data

---
+++ R/states-in-ecoregions.R
@@ -0,0 +1,3 @@
+# Make a df of ecoregions with a list of all the states that each extends into.
+# This will let us just put the ecoregion into the datset builder and it can
+# look up the FIA state data to fetch.
